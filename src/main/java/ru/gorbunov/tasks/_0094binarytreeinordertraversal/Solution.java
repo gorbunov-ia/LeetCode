cdc90562fb790aaa2739c369797afcf7268b6e37
gorbunov-ia <Grbnv.vn@gmail.com>
2020-11-09 22:52:46 +0300

    Binary Tree Inorder Traversal task was added

---
+++ src/main/java/ru/gorbunov/tasks/_0094binarytreeinordertraversal/Solution.java
@@ -0,0 +1,85 @@
+package ru.gorbunov.tasks._0094binarytreeinordertraversal;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Binary Tree Inorder Traversal
+ * <p>
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ * Example 1:
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * Example 2:
+ * Input: root = []
+ * Output: []
+ * Example 3:
+ * Input: root = [1]
+ * Output: [1]
+ * Example 4:
+ * Input: root = [1,2]
+ * Output: [2,1]
+ * Example 5:
+ * Input: root = [1,null,2]
+ * Output: [1,2]
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * Follow up:
+ * Recursive solution is trivial, could you do it iteratively?
+ */
+public class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> output = new LinkedList<>();
+        go(root, output);
+        return output;
+    }
+
+
+    private void go(TreeNode root, List<Integer> output) {
+        if (root == null) {
+            return;
+        }
+        go(root.left, output);
+        output.add(root.val);
+        go(root.right, output);
+    }
+
+    public List<Integer> iterativeInorderTraversal(TreeNode root) {
+        List<Integer> output = new LinkedList<>();
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            output.add(curr.val);
+            curr = curr.right;
+        }
+        return output;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/test/java/ru/gorbunov/tasks/_0094binarytreeinordertraversal/SolutionTest.java
@@ -0,0 +1,71 @@
+package ru.gorbunov.tasks._0094binarytreeinordertraversal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.gorbunov.tasks._0094binarytreeinordertraversal.Solution.TreeNode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void example1() {
+        TreeNode tree = createTree();
+
+        final List<Integer> result = solution.inorderTraversal(tree);
+
+        Assertions.assertEquals(Arrays.asList(1, 3, 2), result);
+    }
+
+    private TreeNode createTree() {
+        TreeNode l3 = new TreeNode(3);
+        TreeNode l2 = new TreeNode(2, l3, null);
+        return new TreeNode(1, null, l2);
+    }
+
+    @Test
+    void example2() {
+        TreeNode tree = null;
+
+        final List<Integer> result = solution.inorderTraversal(tree);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void example3() {
+        TreeNode tree = new TreeNode(1);
+
+        final List<Integer> result = solution.inorderTraversal(tree);
+
+        Assertions.assertEquals(Collections.singletonList(1), result);
+    }
+
+    @Test
+    void example4() {
+        TreeNode tree = new TreeNode(1, new TreeNode(2), null);
+
+        final List<Integer> result = solution.inorderTraversal(tree);
+
+        Assertions.assertEquals(Arrays.asList(2, 1), result);
+    }
+
+    @Test
+    void example5() {
+        TreeNode tree = new TreeNode(1, null, new TreeNode(2));
+
+        final List<Integer> result = solution.inorderTraversal(tree);
+
+        Assertions.assertEquals(Arrays.asList(1, 2), result);
+    }
+}
