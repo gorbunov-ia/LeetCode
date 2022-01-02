529c1adec8ed9527ff1b23b0ec0941acca265484
gorbunov-ia <Grbnv.vn@gmail.com>
2022-01-02 15:33:53 +0300

    Next Permutation task was added

---
+++ src/main/java/ru/gorbunov/tasks/_0031nextpermutation/Solution.java
@@ -0,0 +1,45 @@
+package ru.gorbunov.tasks._0031nextpermutation;
+
+/**
+ * 31. Next Permutation
+ * <p>
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such an arrangement is impossible, it must rearrange it to the lowest possible order (i.e., sorted in
+ * ascending order).
+ * The replacement must be in place and use only constant extra memory.
+ * Example 1:
+ * Input: nums = [1,2,3]
+ * Output: [1,3,2]
+ * Example 2:
+ * Input: nums = [3,2,1]
+ * Output: [1,2,3]
+ * Example 3:
+ * Input: nums = [1,1,5]
+ * Output: [1,5,1]
+ * Constraints:
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ */
+public class Solution {
+
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                swap(nums, i, i - 1);
+                return;
+            }
+        }
+        for (int i = 0; i < nums.length / 2; i++) {
+            swap(nums, i, nums.length - 1 - i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
---
+++ src/test/java/ru/gorbunov/tasks/_0031nextpermutation/SolutionTest.java
@@ -0,0 +1,48 @@
+package ru.gorbunov.tasks._0031nextpermutation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void example1() {
+        int[] nums = new int[]{1, 2, 3};
+
+        solution.nextPermutation(nums);
+
+        Assertions.assertEquals(1, nums[0]);
+        Assertions.assertEquals(3, nums[1]);
+        Assertions.assertEquals(2, nums[2]);
+    }
+
+    @Test
+    void example2() {
+        int[] nums = new int[]{3, 2, 1};
+
+        solution.nextPermutation(nums);
+
+        Assertions.assertEquals(1, nums[0]);
+        Assertions.assertEquals(2, nums[1]);
+        Assertions.assertEquals(3, nums[2]);
+    }
+
+    @Test
+    void example3() {
+        int[] nums = new int[]{1, 1, 5};
+
+        solution.nextPermutation(nums);
+
+        Assertions.assertEquals(1, nums[0]);
+        Assertions.assertEquals(5, nums[1]);
+        Assertions.assertEquals(1, nums[2]);
+    }
+}
