2f9347a68d718fed32337ca6d910f58d687e9b47
gorbunov-ia <Grbnv.vn@gmail.com>
2022-01-04 15:41:04 +0300

    Number of Islands task was added

---
+++ src/test/java/ru/gorbunov/tasks/_0200numberofislands/SolutionTest.java
@@ -0,0 +1,43 @@
+package ru.gorbunov.tasks._0200numberofislands;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void example1() {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+        };
+
+        int result = solution.numIslands(grid);
+
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    void example2() {
+        char[][] grid = new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        int result = solution.numIslands(grid);
+
+        Assertions.assertEquals(3, result);
+    }
+}
